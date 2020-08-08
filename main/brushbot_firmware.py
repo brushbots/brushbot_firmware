cd5e49fe55e032a216ef42b481938a45b043d307
Gennaro Notomista <gnotomista3@gatech.edu>
2020-08-08 20:57:48 +0200

    first OTA test

---
+++ main/brushbot_firmware.py
@@ -0,0 +1,10 @@
+import utime
+from machine import Pin
+
+def run()
+    led = Pin(32, Pin.OUT)
+    while True:
+        led.value(1)
+        utime.sleep(0.5)
+        led.value(0)
+        utime.sleep(0.5)
