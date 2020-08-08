acad445567e2f94505adccba65851024d76670ae
gnotomista <g.notomista@gmail.com>
2020-08-09 01:47:49 +0200

    Update brushbot_firmware.py

--- main/brushbot_firmware.py
+++ main/brushbot_firmware.py
@@ -5,6 +5,6 @@ def run():
     led = Pin(32, Pin.OUT)
     while True:
         led.value(1)
-        utime.sleep(0.5)
+        utime.sleep(5)
         led.value(0)
-        utime.sleep(0.5)
+        utime.sleep(5)
